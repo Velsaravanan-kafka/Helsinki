c82de3d7e58cfc2b4dbbf79104816f7dc2c6a7c7
Vel <velsaravanan204@gmail.com>
2025-07-20 08:28:01 +0530

    1.3

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = (props) => {
+  return (
+    <div>
+
+      <Part part = {props.part1}/>
+      <Part part = {props.part2}/>
+      <Part part = {props.part3}/>
+    {/* <p>{props.part1} {props.exercises1}</p>
+    <p>{props.part2} {props.exercises2}</p>
+    <p>{props.part3} {props.exercises3}</p> */}
+    </div>
+  )
+}
+
+export default Content
---
+++ part1/src/components/Total.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Total = ({totalExercises}) => {
+  return (
+    <p>Number of exercises {totalExercises}</p>
+  )
+}
+
+export default Total
